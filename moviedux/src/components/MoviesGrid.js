a6708dda2fa12011d1fab15f082412e0cf122d40
Maulikdikshit <maulikdikshit013@gmail.com>
2024-12-17 20:06:44 +0530

    feat - Add inital

---
+++ moviedux/src/components/MoviesGrid.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect} from 'react';
+import '../styles.css';
+
+
+export default function MoviesGrid(){
+
+    const [ movies, setMovies ] = useState([]);
+
+    return (
+        <div></div>
+    );
+}
