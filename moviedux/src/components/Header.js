a3ce32f463bc98461195a8e86a86cd24ee8e8f21
Maulikdikshit <maulikdikshit013@gmail.com>
2024-12-22 06:49:34 +0530

    feat - Add second

--- moviedux/src/components/Header.js
+++ moviedux/src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header(){
 
     return (
         <div className='header'>
-            <img className='logo' src='logo512.png' alt='moviedux'></img>
+            <img className='logo' src='logo.png' alt='moviedux'></img>
             <h2 className='app-subtitle'>It's time for popcorn! Find your next movie here</h2>
         </div>
     );
--- moviedux/src/components/MoviesGrid.js
+++ moviedux/src/components/MoviesGrid.js
@@ -1,12 +1,112 @@
-import React, { useState, useEffect} from 'react';
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
 
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
 
-export default function MoviesGrid(){
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [ movies, setMovies ] = useState([]);
+  const [genre, setGenre] = useState("All Genres");
 
+  const [rating, setRating] = useState("All");
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  const handleSearchTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleGenreChange = (e) => {
+    setGenre(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
+  };
+
+  const matchesGenre = (movie, genre) => {
     return (
-        <div></div>
+      genre === "All Genres" ||
+      movie.genre.toLowerCase() === genre.toLowerCase()
     );
+  };
+
+  const matchesRating = (movie, rating) => {
+    switch (rating) {
+      case "All":
+        return true;
+      case "Good":
+        return movie.rating >= 8;
+      case "OK":
+        return movie.rating >= 5 && movie.rating < 8;
+      case "Bad":
+        return movie.rating < 5;
+
+      default:
+        return false;
+    }
+  };
+
+  const matchesSearchTerm = (movies, searchTerm) => {
+    return movies.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+
+  const filteredMovies = movies.filter(
+    (movies) =>
+      matchesGenre(movies, genre) &&
+      matchesRating(movies, rating) &&
+      matchesSearchTerm(movies, searchTerm)
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Movies ..."
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearchTerm}
+      />
+
+      <div className="filter-bar">
+        <div className="filter-slot">
+          <label>Genre</label>
+          <select
+            className="filter-dropdown"
+            value={genre}
+            onChange={handleGenreChange}
+          >
+            <option>All Genres</option>
+            <option>Action</option>
+            <option>Drama</option>
+            <option>Fantasy</option>
+            <option>Horror</option>
+          </select>
+        </div>
+        <div className="filter-slot">
+          <label>Rating</label>
+          <select
+            className="filter-dropdown"
+            value={rating}
+            onChange={handleRatingChange}
+          >
+            <option>All</option>
+            <option>Good</option>
+            <option>Ok</option>
+            <option>Bad</option>
+          </select>
+        </div>
+      </div>
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
